14f680e6888609a99981ff0c3f6c1ee1957384a4
John (Juan) Lema <juan@juanlema.com>
2020-03-21 11:03:21 -0700

    Added CampsiteInfo Component Updated Directory Component to use CampsiteInfo Component

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import CampsiteInfo from './CampsiteInfoComponent';
 
 class Directory extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -11,21 +13,6 @@ class Directory extends Component {
 
     onCampsiteSelect(campsite) {
         this.setState({ selectedCampSite: campsite });
-    }
-
-    renderSelectedCampsite(campsite) {
-        if (campsite) {
-            return (
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            )
-        }
-        return <div />;
     }
 
     render() {
@@ -47,14 +34,11 @@ class Directory extends Component {
                 <div className="row">
                     {directory}
                 </div>
-                <div className="row">
-                    <div className="col-md-5 m-1">
-                        {this.renderSelectedCampsite(this.state.selectedCampSite)}
-                    </div>
-                </div>
+                <CampsiteInfo campsite={this.state.selectedCampSite} />
             </div>
         );
     }
+
 }
 
 export default Directory;
